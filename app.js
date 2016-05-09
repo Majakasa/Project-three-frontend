395c101d8d297b73eec39464a3b3d482ba8d61a6
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-05-09 04:16:45 -0400

    test

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var express = require('express'),
-    app     = express(),
-    PORT    = process.env.PORT || 3000;
-​
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
 app.use(express.static('./dist'));
-​
 app.listen(PORT, function(err) {
   if (err) {
-    console.log('error starting server', err);
+    console.log('error starting server');
+    console.log(err);
   } else {
     console.log('server up and running on port', PORT);
   }
